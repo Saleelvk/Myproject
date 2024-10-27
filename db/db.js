159e5a4a2d4f135655da3b5931b6d66cbd6ec9f0
Saleelvk <87797952+Saleelvk@users.noreply.github.com>
2024-10-27 13:53:02 +0530

    Update db.js

--- db/db.js
+++ db/db.js
@@ -3,8 +3,6 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
     try {
         await mongoose.connect(process.env.Mongo_url, {
-            
-            useUnifiedTopology: true,
         });
         console.log('Database connected');
     } catch (error) {
